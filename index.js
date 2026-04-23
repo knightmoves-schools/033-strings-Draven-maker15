163dd0ca746ce3acc46758170b45b07992694622
Draven-maker15 <draven.davenport@students.msswarriors.org>
2026-04-23 13:58:50 -0500

    Fix syntax error in template string

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ let firstName = "Draven";
 let jobTitle = "Software Engineer";
 let city = "New York";
 
-let newJobPost = `${firstName] is a ${jobTitle} based in ${city}.`
+let newJobPost = `${firstName} is a ${jobTitle} based in ${city}.`
 
 document.getElementById("result").innerHTML = newJobPost;
