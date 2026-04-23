497e2fff95b1deb5341c047f9765d8cdfde0df21
Draven-maker15 <draven.davenport@students.msswarriors.org>
2026-04-22 21:50:36 -0500

    Add job post information to index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+let firstName = "Draven";
+let jobTitle = "Software Engineer";
+let city = "New York";
+
+let newJobPost = `${firstName] is a ${jobTitle} based in ${city}.`;
+
+document.getElementById("result").innerHTML = newJobPost;
